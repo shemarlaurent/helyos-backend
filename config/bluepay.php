39a94efb4f680c4cb4efba2db415dfe8bf41a2f0
shemarlaurent <shemarlaurent1@gmail.com>
2020-06-12 13:33:55 -0400

    first commit

---
+++ config/bluepay.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    "account_id" => env('BP_ACCOUNT_ID', '100806874366'),
+    "secret_key" => env('BP_SECRET_KEY', 'NR.KFYKOTWTTVANINWVDPZFVSV.MDC4Y6'),
+    "mode" => env('BBP_MODE', 'TEST'),
+];
